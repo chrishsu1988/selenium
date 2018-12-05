77acaf239faf3d3a574bad563fa65d71a6884121
chrishsu1988 <59421wind@163.com>
2018-12-05 11:11:49 +0800

    增加base类

---
+++ common/base.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# 基类
+class Base():
+    def __init__(self, driver: webdriver.Firefox()):
+        self.driver = driver
+
+# 查找单元素的方法
+    def find(self, locator, timeout=30):
+        element = WebDriverWait(self.driver, timeout, 0.5).until(EC.presence_of_element_located(locator))
+        return element
+
+# 查找多个元素的方法
+    def finds(self, locator, timeout=30):
+        elements = WebDriverWait(self.driver, timeout, 0.5).until(EC.presence_of_element_located(locator))
+        return elements
+
+# 输入文本的方法
+    def send(self, locator, text):
+        element = self.find(locator)
+        element.send_keys(text)
+
+# 模拟鼠标点击的方法
+    def click(self, locator):
+        element = self.find(locator)
+        element.click()
+
+# 清空输入框的方法
+    def clear(self, locator):
+        element = self.find(locator)
+        element.clear()
+
+# 获取文本的方法
+    def get_text(self, locator):
+        try:
+            element = self.find(locator)
+            t = element.text
+            return t
+        except:
+            return "" 
